77bc794ed80d83289acc39c4175ed0639e4b3249
Becca <rrdoukas@gmail.com>
2020-05-20 17:46:30 -0500

    adds github solution

---
+++ git_and_github_solution.rb
@@ -0,0 +1,21 @@
+# Paste a link to your github repository below:
+<<<<<<< HEAD
+https://github.com/irvingvillasenor?tab=repositories
+
+#https://github.com/RDoukas/hello_world
+=======
+>>>>>>> 20875b937d5a7345439d70515aea294cf1ce4bca
+
+<<<<<<< HEAD
+
+# What questions do you have about what we've learned so far using git and github?:
+
+# Most of it I have experience with, I've used git and github in the past. If there's anything I'm not executing correctly please let me know!
+
+=======
+# What questions do you have about what we've learned so far using git and github?:
+<<<<<<< HEAD
+1) At the moment i do not have any questions as your instruction and directions have been pretty clear. I look forward to seeing what more i learn.
+>>>>>>> 2e7b14fa2bf1911cd4e19350c6f7d0ac00aebc7b
+=======
+>>>>>>> 20875b937d5a7345439d70515aea294cf1ce4bca
